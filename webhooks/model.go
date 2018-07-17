5801422ab588348e86d176867248b61b70dc3fa2
Rafael de Aquino Cunha <aquinocunha@gmail.com>
2018-07-17 14:48:43 -0300

    Adicionada a estrutura para webhooks.

---
+++ webhooks/model.go
@@ -0,0 +1,18 @@
+package webhooks
+
+// Data - Estrutura do campo data do webhook
+type Data struct {
+	ID string `json:"id"`
+}
+
+// Webhook - Estrutura do webhook
+type Webhook struct {
+	ID          int64  `json:"id"`
+	LiveMode    bool   `json:"live_mode"`
+	Type        string `json:"type"`
+	DateCreated string `json:"date_created"`
+	UserID      int64  `json:"user_id"`
+	APIVersion  string `json:"api_version"`
+	Action      string `json:"action"`
+	Data        Data   `json:"data"`
+}
