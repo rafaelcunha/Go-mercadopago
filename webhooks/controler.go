f76df693293326373cb26ad943e42b3e05b78087
Rafael de Aquino Cunha <aquinocunha@gmail.com>
2018-07-18 10:27:50 -0300

    webhooks

--- webhooks/controler.go
+++ webhooks/controler.go
@@ -1,1 +1,45 @@
 package webhooks
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// WebhookJSONDecoder - Decodigica o webhook de pagamento.
+func WebhookJSONDecoder(r *http.Request) (*Webhook, error) {
+	decoder := json.NewDecoder(r.Body)
+	var webhook Webhook
+	err := decoder.Decode(&webhook)
+
+	if err != nil {
+		log.Println("webhookJsonDecoder - falha ao gerar estrutura do Webhook recebido.")
+		return nil, err
+	}
+
+	return &webhook, nil
+}
+
+// TrataWebhook - Trata uma chamada de webhook recebida
+/*func TrataWebhook(w http.ResponseWriter, r *http.Request) (err error) {
+
+	parametros := r.URL.Query() // Returns a url.Values, which is a map[string][]string
+
+	log.Println("Parametros recebidos: ")
+
+	for key, value := range parametros {
+		log.Println("Key: ", key, "Value: ", value)
+	}
+
+	tipo := parametros.Get("type")
+
+	switch tipo {
+	case "payment":
+
+		break
+	default:
+		err = errors.New("Tipo de webhook sem tratamento definido. Tipo: " + tipo)
+	}
+
+	return err
+}*/
